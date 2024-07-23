62020456db520181182c0ec6c04886eaf304cfe5
Samir <samirkumar1070@gmail.com>
2024-07-23 14:50:42 +0530

    user frontend modified

--- user-portal-frontend/src/index.js
+++ user-portal-frontend/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
